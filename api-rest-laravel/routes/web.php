ddb5f493ff0f88b38704b80ffb1ffaaae11981a4
Mirko González <mirkohijo@gmail.com>
2020-04-28 19:43:20 +0200

    Actualización de Rutas!
    cambios menores, pero relevantes!

--- api-rest-laravel/routes/web.php
+++ api-rest-laravel/routes/web.php
@@ -4,13 +4,22 @@
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
 */
 
 Route::get('/', function () {
+    return '<h1>Hola Mundo con Laravel</h1>';
+});
+
+Route::get('/welcome', function () {
     return view('welcome');
 });
+
+Route::get('/pruebas/{nombre?}', function ($nombre = null) {
+    $texto = 'Texto desde una ruta con nombre →';
+    $texto .= ' ' . $nombre;
+   return view('pruebas', array(
+        'texto' => $texto
+    ));
+});
+
+Route::get('/animales', 'PruebasController@index');
